2aa06ddecc1de124ff52bf9a568512b7f42ed71c
Johan Decorte <johan.decorte@hogent.be>
2025-03-07 16:07:39 +0100

    last exercise

---
+++ training2.c
@@ -0,0 +1,45 @@
+/* A function performs an operation on two doubles, which are passed as parameters. 
+The function returns a double containing the result of the operation. 
+In addition, it is also specified which operation should be performed: 
+addition, subtraction, multiplication, etc. Implement this in a 
+flexible way so that additional operations can be added later
+without having to modify the function.
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+void showBinary(uint8_t n)
+{
+	for (int i = 7; i >= 0; i--)
+	{
+		printf("%d", (n >> i) & 1);
+	}
+}
+
+int main()
+{
+	uint8_t number;
+	uint8_t choice, bit_position;
+	printf("Enter a number (0-255): ");
+	scanf_s("%hhu", &number);
+	// show operations:
+	printf("\Choose an operation:\n");
+	printf("1. Set bit\n");
+	printf("2. Clear bit\n");
+	printf("3. Read bit\n");
+	printf("4. Toggle bit\n");
+	printf("Your choice: ");
+	scanf_s("%hhu", &choice);
+	printf("Bit position (0-7): ");
+	scanf_s("%hhu", &bit_position);
+	if (bit_position < 0 || bit_position >7)
+	{
+		printf("Invalid bit position!");
+		return 1;
+	}
+	showBinary(number);
+
+
+	return 0;
+	}
